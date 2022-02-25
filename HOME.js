1d2de43ffdbbabdc482b6529fc921e37999a7a09
ritik <ritiksharma0723@gmail.com>
2022-02-25 22:18:49 +0530

    comited
    comited

---
+++ HOME.js
@@ -0,0 +1,45 @@
+    var counter = 1;
+    setInterval(function(){
+      document.getElementById('radio' + counter).checked = true;
+      counter++;
+      if(counter > 6){
+        counter = 1;
+      }
+    }, 5000);
+    document.getElementById("next").addEventListener("click",next);
+    document.getElementById("pre").addEventListener("click",pre);
+    function next(){
+      var a=document.getElementById("image");
+      var b=document.getElementById("next");
+      var c=document.getElementById("pre");
+      a.style.left="-495px";
+      b.style.visibility="hidden";
+      c.style.visibility="visible";
+    }
+    function pre(){
+      var a=document.getElementById("image");
+      var b=document.getElementById("next");
+      var c=document.getElementById("pre");
+      a.style.left="0px";
+      b.style.visibility="visible";
+      c.style.visibility="hidden";
+    }
+
+    document.getElementById("next2").addEventListener("click",next2);
+    document.getElementById("pre2").addEventListener("click",pre2);
+    function next2(){
+      var a=document.getElementById("mobile_image");
+      var b=document.getElementById("next2");
+      var c=document.getElementById("pre2");
+      a.style.left="-800px";
+      b.style.visibility="hidden";
+      c.style.visibility="visible";
+    }
+    function pre2(){
+      var a=document.getElementById("mobile_image");
+      var b=document.getElementById("next2");
+      var c=document.getElementById("pre2");
+      a.style.left="-250px";
+      b.style.visibility="visible";
+      c.style.visibility="hidden";
+    }
