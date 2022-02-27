f4794bbcea33510469950b9ff62e7a541519c43f
ritik <ritiksharma0723@gmail.com>
2022-02-27 08:46:14 +0530

    commit

--- HOME.js
+++ HOME.js
@@ -1,11 +1,11 @@
-    // var counter = 1;
-    // setInterval(function(){
-    //   document.getElementById('radio' + counter).checked = true;
-    //   counter++;
-    //   if(counter > 6){
-    //     counter = 1;
-    //   }
-    // }, 5000);
+    var counter = 1;
+    setInterval(function(){
+      document.getElementById('radio' + counter).checked = true;
+      counter++;
+      if(counter > 6){
+        counter = 1;
+      }
+    }, 5000);
     document.getElementById("next").addEventListener("click",next);
     document.getElementById("pre").addEventListener("click",pre);
     function next(){
