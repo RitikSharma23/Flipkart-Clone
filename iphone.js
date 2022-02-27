f72aa3ebea486488bafad10615c99a8fe1f967f8
ritik <ritiksharma0723@gmail.com>
2022-02-27 21:06:25 +0530

    commit

--- iphone.js
+++ iphone.js
@@ -24,3 +24,37 @@ function a5(){
     var a=document.getElementById("photo");
     a.style.backgroundImage="url(icons/Iphone_icons/5.jpeg)";
 }
+
+
+
+
+
+
+
+
+document.getElementById("next2").addEventListener("click",next2);
+    document.getElementById("pre2").addEventListener("click",pre2);
+    function next2(){
+      document.getElementById("iphone").style.visibility="hidden"
+      var a=document.getElementById("div2");
+      var b=document.getElementById("next2");
+      var c=document.getElementById("pre2");
+      a.style.left="-47vw";
+      b.style.visibility="hidden";
+      c.style.visibility="visible";
+    }
+    function pre2(){
+      var a=document.getElementById("div2");
+      var b=document.getElementById("next2");
+      var c=document.getElementById("pre2");
+      a.style.left="0px";
+      b.style.visibility="visible";
+      c.style.visibility="hidden";
+      ;
+      
+    }
+    
+
+   function time(){
+    document.getElementById("iphone").style.visibility="visible"
+   }
