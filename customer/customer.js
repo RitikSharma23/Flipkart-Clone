5353972fa9cad71c65864aa81643571e272356c6
Vrutik11 <100480211+Vrutik11@users.noreply.github.com>
2022-05-13 00:21:08 +0530

    commit

---
+++ customer/customer.js
@@ -0,0 +1,153 @@
+
+    document.getElementById("flip").addEventListener("click",()=>{
+    document.getElementById("flipdiv").style.display = "block"
+    document.getElementById("orderdiv").style.display = "none"
+    document.getElementById("canceldiv").style.display = "none"
+    document.getElementById("paydiv").style.display = "none"
+    document.getElementById("shopdiv").style.display = "none"
+    document.getElementById("walldiv").style.display = "none"
+    document.getElementById("shipdiv").style.display = "none"
+    document.getElementById("insdiv").style.display = "none"
+    document.getElementById("quidiv").style.display = "none"
+    document.getElementById("coindiv").style.display = "none"
+    document.getElementById("refdiv").style.display = "none"
+    })
+    document.getElementById("order").addEventListener("click", () => {
+    document.getElementById("flipdiv").style.display = "none"
+    document.getElementById("orderdiv").style.display = "block"
+    document.getElementById("canceldiv").style.display = "none"
+    document.getElementById("paydiv").style.display = "none"
+    document.getElementById("shopdiv").style.display = "none"
+    document.getElementById("walldiv").style.display = "none"
+    document.getElementById("shipdiv").style.display = "none"
+    document.getElementById("insdiv").style.display = "none"
+    document.getElementById("quidiv").style.display = "none"
+    document.getElementById("coindiv").style.display = "none"
+    document.getElementById("refdiv").style.display = "none"
+})
+
+    document.getElementById("cancel").addEventListener("click", () => {
+    document.getElementById("flipdiv").style.display = "none"
+    document.getElementById("orderdiv").style.display = "none"
+    document.getElementById("canceldiv").style.display = "block"
+    document.getElementById("paydiv").style.display = "none"
+    document.getElementById("shopdiv").style.display = "none"
+    document.getElementById("walldiv").style.display = "none"
+    document.getElementById("shipdiv").style.display = "none"
+    document.getElementById("insdiv").style.display = "none"
+        document.getElementById("quidiv").style.display = "none"
+    document.getElementById("coindiv").style.display = "none"
+    document.getElementById("refdiv").style.display = "none"
+})
+
+    document.getElementById("pay").addEventListener("click", () => {
+    document.getElementById("flipdiv").style.display = "none"
+    document.getElementById("orderdiv").style.display = "none"
+    document.getElementById("canceldiv").style.display = "none"
+    document.getElementById("paydiv").style.display = "block"
+    document.getElementById("shopdiv").style.display = "none"
+    document.getElementById("walldiv").style.display = "none"
+    document.getElementById("shipdiv").style.display = "none"
+    document.getElementById("insdiv").style.display = "none"
+    document.getElementById("quidiv").style.display = "none"
+    document.getElementById("coindiv").style.display = "none"
+    document.getElementById("refdiv").style.display = "none"
+})
+
+document.getElementById("shop").addEventListener("click", () => {
+    document.getElementById("flipdiv").style.display = "none"
+    document.getElementById("orderdiv").style.display = "none"
+    document.getElementById("canceldiv").style.display = "none"
+    document.getElementById("paydiv").style.display = "none"
+    document.getElementById("shopdiv").style.display = "block"
+    document.getElementById("walldiv").style.display = "none"
+    document.getElementById("shipdiv").style.display = "none"
+    document.getElementById("insdiv").style.display = "none"
+    document.getElementById("quidiv").style.display = "none"
+    document.getElementById("coindiv").style.display = "none"
+    document.getElementById("refdiv").style.display = "none"
+})
+
+    document.getElementById("wall").addEventListener("click", () => {
+    document.getElementById("flipdiv").style.display = "none"
+    document.getElementById("orderdiv").style.display = "none"
+    document.getElementById("canceldiv").style.display = "none"
+    document.getElementById("paydiv").style.display = "none"
+    document.getElementById("shopdiv").style.display = "none"
+    document.getElementById("walldiv").style.display = "block"
+    document.getElementById("shipdiv").style.display = "none"
+    document.getElementById("insdiv").style.display = "none"
+        document.getElementById("quidiv").style.display = "none"
+    document.getElementById("coindiv").style.display = "none"
+    document.getElementById("refdiv").style.display = "none"
+})
+
+    document.getElementById("oth").addEventListener("click", () => {
+    document.getElementById("flipdiv").style.display = "none"
+    document.getElementById("orderdiv").style.display = "none"
+    document.getElementById("canceldiv").style.display = "none"
+    document.getElementById("paydiv").style.display = "none"
+    document.getElementById("shopdiv").style.display = "none"
+    document.getElementById("walldiv").style.display = "none"
+    document.getElementById("shipdiv").style.display = "block"
+    document.getElementById("insdiv").style.display = "none"
+    document.getElementById("quidiv").style.display = "none"
+    document.getElementById("coindiv").style.display = "none"
+    document.getElementById("refdiv").style.display = "none"
+})
+
+    document.getElementById("ins").addEventListener("click", () => {
+    document.getElementById("flipdiv").style.display = "none"
+    document.getElementById("orderdiv").style.display = "none"
+    document.getElementById("canceldiv").style.display = "none"
+    document.getElementById("paydiv").style.display = "none"
+    document.getElementById("shopdiv").style.display = "none"
+    document.getElementById("walldiv").style.display = "none"
+    document.getElementById("shipdiv").style.display = "none"
+    document.getElementById("insdiv").style.display = "block"
+        document.getElementById("quidiv").style.display = "none"
+    document.getElementById("coindiv").style.display = "none"
+    document.getElementById("refdiv").style.display = "none"
+})
+
+document.getElementById("qui").addEventListener("click", () => {
+    document.getElementById("flipdiv").style.display = "none"
+    document.getElementById("orderdiv").style.display = "none"
+    document.getElementById("canceldiv").style.display = "none"
+    document.getElementById("paydiv").style.display = "none"
+    document.getElementById("shopdiv").style.display = "none"
+    document.getElementById("walldiv").style.display = "none"
+    document.getElementById("shipdiv").style.display = "none"
+    document.getElementById("insdiv").style.display = "none"
+    document.getElementById("quidiv").style.display = "block"
+    document.getElementById("coindiv").style.display = "none"
+    document.getElementById("refdiv").style.display = "none"
+})
+
+    document.getElementById("coin").addEventListener("click", () => {
+    document.getElementById("flipdiv").style.display = "none"
+    document.getElementById("orderdiv").style.display = "none"
+    document.getElementById("canceldiv").style.display = "none"
+    document.getElementById("paydiv").style.display = "none"
+    document.getElementById("shopdiv").style.display = "none"
+    document.getElementById("walldiv").style.display = "none"
+    document.getElementById("shipdiv").style.display = "none"
+    document.getElementById("insdiv").style.display = "none"
+        document.getElementById("quidiv").style.display = "none"
+    document.getElementById("coindiv").style.display = "block"
+    document.getElementById("refdiv").style.display = "none"
+})
+
+   document.getElementById("ref").addEventListener("click", () => {
+    document.getElementById("flipdiv").style.display = "none"
+    document.getElementById("orderdiv").style.display = "none"
+    document.getElementById("canceldiv").style.display = "none"
+    document.getElementById("paydiv").style.display = "none"
+    document.getElementById("shopdiv").style.display = "none"
+    document.getElementById("walldiv").style.display = "none"
+    document.getElementById("shipdiv").style.display = "none"
+    document.getElementById("insdiv").style.display = "none"
+       document.getElementById("quidiv").style.display = "none"
+    document.getElementById("coindiv").style.display = "none"
+    document.getElementById("refdiv").style.display = "block"
+})
